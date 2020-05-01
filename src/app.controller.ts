371a87ae8a712e5ccac5107a95d984f33b63651f
Nikola Djikic <nikoladjikic22@gmail.com>
2020-05-01 21:09:54 +0200

    Made some changes to the main app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,12 +5,7 @@ export class AppController {
   constructor() { }
 
   @Get() // http://localhost:3000/
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  @Get('world') // http://localhost:3000/world/
-  getWorld(): string {
-    return 'World!!!';
+  getIndex(): string {
+    return 'Homepage!';
   }
 }
